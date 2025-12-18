937bf21960ccf60b355c8da03fc03fa686a69c8b
jwong65 <jason.wong6507@gmail.com>
2025-12-18 04:02:00 -0500

    Added script to player for gravity and movement.

---
+++ player/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var SPEED := 300.0
+@export var JUMP_VELOCITY := -400.0
+@export var GRAVITY := 1200.0
+
+func _physics_process(delta: float) -> void:
+	# Apply gravity
+	if not is_on_floor():
+#		This basically means falling faster every frame for gravity. Delta is time since last frame
+		velocity.y += GRAVITY * delta
+	else:
+#		You are on the floor so you cannot be falling.
+		velocity.y = 0
+
+	# Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Horizontal movement
+	var direction := Input.get_axis("move_left", "move_right")
+	velocity.x = direction * SPEED
+
+	move_and_slide()
