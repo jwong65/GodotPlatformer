cd6f714e09588624c9be61d805a5d400e42d00ca
jwong65 <jason.wong6507@gmail.com>
2025-12-23 02:20:35 -0500

    adjusted statements so that they cannot be played when current_attack is true

--- player/player.gd
+++ player/player.gd
@@ -10,24 +10,27 @@ var current_attack: bool
 #This is the ready function that is initalized at the start.
 func _ready() -> void:
 	Global.playerBody = self
+	current_attack = false
 	
 func handlePlayerAnimation(direction: float)-> void :
 	if direction != 0:
 		sprite.flip_h = direction <0
 
-	if not is_on_floor():
+	if not is_on_floor() and !current_attack:
 		if velocity.y < 0:
 			sprite.play("JumpUp")
 		else:
 			sprite.play("JumpDown")
 		return 
 
-	if direction == 0:
+	if direction == 0 and !current_attack:
 		sprite.play("Idle")
 	else:
 		sprite.play("Walk")
-	
-	
+		
+func handle_attack_animation(attack_type: String):
+	pass
+
 func _physics_process(delta: float) -> void:
 	# Apply gravity
 	if not is_on_floor():
@@ -41,10 +44,14 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-
 	# Horizontal movement
 	var direction := Input.get_axis("move_left", "move_right")
 	velocity.x = direction * SPEED
-
+	
+	if !current_attack and Input.is_action_just_pressed("attack"):
+		current_attack = true
+		attack_type = 'single'
+		handle_attack_animation(attack_type)
+		
 	handlePlayerAnimation(direction)
 	move_and_slide()
