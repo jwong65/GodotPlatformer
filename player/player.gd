305ce9625e1946764d4ca9ba6b1a76413d83c4a1
jwong65 <jason.wong6507@gmail.com>
2025-12-23 00:22:26 -0500

    Connected the animation, setup the hitbox with the collisionshape

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var attack_hitbox: CollisionShape2D =$AttackHitbox/CollisionShape2D
 @export var SPEED := 300.0
 @export var JUMP_VELOCITY := -400.0
 @export var GRAVITY := 1200.0
@@ -13,10 +14,12 @@ func _ready() -> void:
 func handlePlayerAnimation(direction: float)-> void :
 	if direction != 0:
 		sprite.flip_h = direction <0
-#	Attack animation?
-	if is_attacking == true:
-		sprite.play()
-		
+		#	Attack animation
+	if is_attacking:
+		sprite.play("Attack")
+		attack_hitbox.disabled = false
+		return  # skip other animations while attacking
+
 	if not is_on_floor():
 		if velocity.y < 0:
 			sprite.play("JumpUp")
@@ -42,6 +45,9 @@ func _physics_process(delta: float) -> void:
 	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	# Attacking
+	if Input.is_action_just_pressed("attack") and !is_attacking:
+		is_attacking= true
 
 	# Horizontal movement
 	var direction := Input.get_axis("move_left", "move_right")
@@ -52,4 +58,12 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_attack_hitbox_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+#	The enemies should be in group->enemies (seen by going to the node and selecting a group)
+	if body.is_in_group("enemies"):
+		body.take_damage(1) 
+
+#This is to disable the attacking hitbox after the animation is finished, also reseting the is_attacking variable
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if sprite.animation == "Attack":
+		attack_hitbox.disabled = true
+		is_attacking = false
