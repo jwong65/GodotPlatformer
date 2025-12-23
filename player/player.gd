46b9f264f3a39845b427dd7ebaeb88a3d717df0f
jwong65 <jason.wong6507@gmail.com>
2025-12-23 02:24:13 -0500

    removed all mentions of attack in script

--- player/player.gd
+++ player/player.gd
@@ -1,36 +1,31 @@
 extends CharacterBody2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var attack_hitbox: CollisionShape2D =$AttackHitbox/CollisionShape2D
+#@onready var attack_hitbox: CollisionShape2D =$AttackHitbox/CollisionShape2D
 @export var SPEED := 300.0
 @export var JUMP_VELOCITY := -400.0
 @export var GRAVITY := 1200.0
 
-var attack_type: String
-var current_attack: bool
 #This is the ready function that is initalized at the start.
 func _ready() -> void:
 	Global.playerBody = self
-	current_attack = false
-	
+		
 func handlePlayerAnimation(direction: float)-> void :
 	if direction != 0:
 		sprite.flip_h = direction <0
 
-	if not is_on_floor() and !current_attack:
+	if not is_on_floor():
 		if velocity.y < 0:
 			sprite.play("JumpUp")
 		else:
 			sprite.play("JumpDown")
 		return 
 
-	if direction == 0 and !current_attack:
+	if direction == 0:
 		sprite.play("Idle")
 	else:
 		sprite.play("Walk")
-		
-func handle_attack_animation(attack_type: String):
-	pass
-
+	
+	
 func _physics_process(delta: float) -> void:
 	# Apply gravity
 	if not is_on_floor():
@@ -47,11 +42,6 @@ func _physics_process(delta: float) -> void:
 	# Horizontal movement
 	var direction := Input.get_axis("move_left", "move_right")
 	velocity.x = direction * SPEED
-	
-	if !current_attack and Input.is_action_just_pressed("attack"):
-		current_attack = true
-		attack_type = 'single'
-		handle_attack_animation(attack_type)
-		
+
 	handlePlayerAnimation(direction)
 	move_and_slide()
