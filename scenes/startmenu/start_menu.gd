194b5a2542a90f9c0a162ca4cf6e1cb0e95cb988
jwong65 <jason.wong6507@gmail.com>
2026-01-05 06:10:20 -0500

    Added comments for future level transition from Start Menu.

--- scenes/startmenu/start_menu.gd
+++ scenes/startmenu/start_menu.gd
@@ -12,8 +12,9 @@ func _process(delta: float) -> void:
 
 
 func _on_start_game_pressed() -> void:
+	#await LevelTransition.fade_to_black()
 	get_tree().change_scene_to_file("res://scenes/world/TestLevel.tscn")
-	
+	#LevelTransition.fade_from_black()
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	RenderingServer.set_default_clear_color(Color.BLACK)
 	pass # Replace with function body.
 
 
